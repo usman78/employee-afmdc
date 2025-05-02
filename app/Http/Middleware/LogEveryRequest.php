fa75c2f99896941a81070b23a57f252e703989e9
usman78 <iam.usmanali1@gmail.com>
2025-05-02 11:03:04 +0500

    created leave approvals feature & advance leave application and discarded the old attendance leave application

---
+++ app/Http/Middleware/LogEveryRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Log;
+
+class LogEveryRequest
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        Log::info('Request:', [
+            'url' => $request->url(),
+            'method' => $request->method(),
+            'ip' => $request->ip(),
+        ]);
+
+        return $next($request);
+    }
+}
