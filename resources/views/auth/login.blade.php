d8f61062ac26ed759bfaa97dafa573387580ff2a
usman78 <iam.usmanali1@gmail.com>
2025-05-02 10:53:33 +0500

    centered the login error message

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -172,7 +172,7 @@
                                 @if ($errors->any())
                                     <div class="alert alert-danger">
                                         @foreach ($errors->all() as $error)
-                                            <div>{{ $error }}</div>
+                                            <div style="text-align: center;">{{ $error }}</div>
                                         @endforeach
                                     </div>
                                 @endif
