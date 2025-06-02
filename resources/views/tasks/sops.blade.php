3baf96694bfa8ccec6a24b7f32f7268cbc242b07
usman78 <iam.usmanali1@gmail.com>
2025-06-02 10:53:18 +0500

    Added the tasks tab in the app

---
+++ resources/views/tasks/sops.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+@section('content')
+    <section id="services" class="services section">
+
+      <!-- Section Title -->
+      <div class="container section-title aos-init aos-animate" data-aos="fade-up">
+        <h2>Tasks</h2>
+        <p>View the minutes of meetings, tasks assigned to you and the SOP's of your department.</p>
+      </div><!-- End Section Title -->
+
+      <div class="container">
+      </div>
+    </section>
+@endsection      
