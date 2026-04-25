4075d995c73f2fe94d088ac762782efe1e66b127
usman78 <iam.usmanali1@gmail.com>
2026-04-25 12:06:28 +0500

    login favicon

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="Login to Employee Portal of Aziz Fatimah Medical &amp; Dental College">
         <meta name="author" content="AFMDC IT Department">
+        <!-- Favicons -->
         <title>Employee Portal AFMDC - Login</title>
+        <link href="{{asset("/img/AFMDC-Logo.png")}}" rel="icon">
         <!-- Custom fonts for this template-->
         <link href="{{ asset('sb/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
